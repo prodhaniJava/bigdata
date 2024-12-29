d09b42b0ad8043e8e6cabd8d4edfbff81d9cce63
Rezaul Prodhani <rezaulprodhani@172-11-17-44.lightspeed.irvnca.sbcglobal.net>
2024-12-29 23:56:57 +0530

    hadoop compatible changes

--- bigdatalearning/src/main/java/com/prodhani/bigdata/mapreduce/country/AgeCountryAnalysis.java
+++ bigdatalearning/src/main/java/com/prodhani/bigdata/mapreduce/country/AgeCountryAnalysis.java
@@ -1,22 +1,26 @@
 package com.prodhani.bigdata.mapreduce.country;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
-public class AgeCountryAnalysis {
+public class AgeCountryAnalysis extends Configured implements Tool  {
 
-    public static void main(String[] args) throws Exception {
+	@Override
+    public int run(String[] args) throws Exception {
         if (args.length != 2) {
             System.err.println("Usage: AgeCountryAnalysis <input path> <output path>");
-            System.exit(-1);
+            return -1;
         }
-
-        Configuration conf = new Configuration();
+        // Reuse the configuration passed from main
+        Configuration conf = getConf();
         Job job = Job.getInstance(conf, "Age Country Analysis");
         job.setJarByClass(AgeCountryAnalysis.class);
         job.setMapperClass(AgeCountryMapper.class);
@@ -25,6 +29,13 @@ public class AgeCountryAnalysis {
         job.setOutputValueClass(IntWritable.class);
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
+
+        return job.waitForCompletion(true) ? 0 : 1;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        int res = ToolRunner.run(conf, new AgeCountryAnalysis(), args);
+        System.exit(res);
     }
 }
